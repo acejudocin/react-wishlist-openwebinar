2ecf9e0f24c9936f28a37cb4ddecd0e74e92327e
Antonio Cejudo <a.cejudocintas@gmail.com>
2020-04-05 14:01:43 +0200

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
-const App = () => <div>My wishlist app</div>
+
+const wishes = [
+    { text: 'Travel to the moon', done: false },
+    { text: 'Pay the gym', done: true },
+    { text: 'Go to the gym', done: false },
+];
+let newWish = '';
+
+const addWishToList = () => {
+    wishes.add(newWish);
+    this.newWish = '';
+}
+
+const App = () =>
+    <div>
+        <h1>My wishlist</h1>
+        <input placeholder="Enter your wish" />
+        <ul>
+            {wishes.map(({ text, done }, i) => (
+                <li>
+                    <label htmlFor={`wish${i}`}>
+                        <input id={`wish${i}`} type="checkbox" checked={done} />
+                        {text}
+                    </label>
+                </li>
+            ))}
+        </ul>
+        <button onClick="addWishToList()">Add wish</button>
+    </div>
 
 export default App;
